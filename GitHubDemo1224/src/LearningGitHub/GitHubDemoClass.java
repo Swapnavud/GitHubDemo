b90c64d88037ebb9c547776fd97603080f5f19f8
Swapnavud <swapnavudutha@gmail.com>
2017-12-24 13:51:38 -0600

    Added another print statement.

--- GitHubDemo1224/src/LearningGitHub/GitHubDemoClass.java
+++ GitHubDemo1224/src/LearningGitHub/GitHubDemoClass.java
@@ -5,7 +5,7 @@ public class GitHubDemoClass {
 	public static void main(String[] args) {
 	
 		System.out.println("Initial Project output");
-		
+		System.out.println("First commit - Test");
 
 	}
 
